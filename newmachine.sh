e5dbd3afb67561252d985079bbac63fd6ddb3da4
chainsawriot <chainsawtiney@gmail.com>
2015-05-02 14:02:06 +0800

    Newmachine.sh

---
+++ newmachine.sh
@@ -0,0 +1,10 @@
+echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" | tee -a /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+apt-get update
+apt-get install hime libreoffice gimp python-pip python-dev build-essential emacs24 im-switch r-base r-base-dev vlc lubuntu-restricted-extras vinagre -y
+apt-get install ess -y
+apt-get install terminator -y
+pip install --upgrade pip 
+pip install --upgrade virtualenv 
+apt-get remove abiword gnumeric xpad lxterminal
+sudo chown chainsaw /usr/local/lib/R/site-library/
