75d35fa7bf6bf44f108fedec2c34358d40a1420b
chainsawriot <chainsawtiney@gmail.com>
2015-05-26 12:32:14 +0800

    really all packages for the meta-analysis project

--- install_rpkg.R
+++ install_rpkg.R
@@ -1,1 +1,3 @@
-install.packages(c("stringr", "metafor", "lubridate", "plyr", "rio", "metafor", "dplyr", "magrittr"))
+install.packages(c("stringr", "metafor", "lubridate", "plyr", "rio", "metafor", "dplyr", "magrittr", "devtools"))
+devtools::install_github("jennybc/googlesheets")
+
