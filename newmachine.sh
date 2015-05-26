2ff54a683363fe2c7b0a64ee86ddc273332c4921
chainsawriot <chainsawtiney@gmail.com>
2015-05-26 11:16:02 +0800

    Install at least the R packages for the meta-analysis project

--- newmachine.sh
+++ newmachine.sh
@@ -2,8 +2,8 @@ echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" | tee -a /etc/apt/so
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update
 apt-get install hime libreoffice gimp python-pip python-dev build-essential emacs24 im-switch r-base r-base-dev vlc lubuntu-restricted-extras vinagre -y
-apt-get install ess -y
-apt-get install terminator -y
+apt-get install ess emacs-goodies-el -y
+apt-get install terminator libcurl4-openssl-dev libxml2-dev libpq-dev -y
 pip install --upgrade pip 
 pip install --upgrade virtualenv 
 apt-get remove abiword gnumeric xpad lxterminal
--- newmachine_nonroot.sh
+++ newmachine_nonroot.sh
@@ -1,1 +1,3 @@
 im-switch -s hime
+echo "alias emacs='emacs -nw'" >> ~/.bashrc
+Rscript install_rpkg.R
